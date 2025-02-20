44c92f51b9c0217976497bd034e4346d2819f7da
ChangWook Song <90672619+songcw8@users.noreply.github.com>
2025-02-20 17:08:59 +0900

    이름변경

--- Bot.java
+++ Bot.java
@@ -15,7 +15,7 @@ public class Bot {
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(webhookUrl))
             .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
+            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"너 누기야\"}")) 
             .build();
 
         try{
