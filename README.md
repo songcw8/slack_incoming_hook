fc46586c3d9f91d525c73a0ae89029ca9348c782
ChangWook Song <90672619+songcw8@users.noreply.github.com>
2025-02-20 16:57:16 +0900

    basic code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # slack_incoming_hook
+
+```
+- bot.java
+- .github/workflows/send-message.yml
+```
---
+++ bot.java
@@ -0,0 +1,31 @@
+import java.net.*;
+import java.net.http.*;
+import java.time.*;
+import java.util.*;
+
+public class Bot {
+    public static void main(String[] args){
+        // 웹훅을 만들거임 -> url 필요함
+        // 환경변수로 받아올 것임 -> yml 파일에서 전달하게
+        String webHookUrl = System.getnev("SLACK_WEBHOOK_URL");
+
+        // Java 11 -> fetch
+        HttpClient client = HttpClient.newHttpClient();
+        //요청을 얹힐거다
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(webhookUrl))
+            .header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
+            .build();
+
+        try{
+            HttpResponse<String> response = client.send(;
+                request, HttpResponse.BodyPublishers.ofString()
+            );
+            System.out.println("요청 코드: " + response.statusCode());
+            System.out.println("응답 결과: " + response.body());
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
